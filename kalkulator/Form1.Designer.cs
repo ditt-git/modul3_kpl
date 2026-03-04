f4f8b70d9d628ffe9eb01415f07448ad6c1a5407
Agung Bramasta <jayabramagung@gmail.com>
2026-03-04 14:27:04 +0700

    first commit

---
+++ kalkulator/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            textBox1 = new TextBox();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(29, 134);
+            button1.Name = "button1";
+            button1.Size = new Size(82, 58);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(146, 134);
+            button2.Name = "button2";
+            button2.Size = new Size(82, 58);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(258, 134);
+            button3.Name = "button3";
+            button3.Size = new Size(82, 58);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(29, 240);
+            button4.Name = "button4";
+            button4.Size = new Size(82, 58);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(146, 240);
+            button5.Name = "button5";
+            button5.Size = new Size(82, 58);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(258, 240);
+            button6.Name = "button6";
+            button6.Size = new Size(82, 58);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(29, 336);
+            button7.Name = "button7";
+            button7.Size = new Size(82, 58);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(146, 336);
+            button8.Name = "button8";
+            button8.Size = new Size(82, 58);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(258, 336);
+            button9.Name = "button9";
+            button9.Size = new Size(82, 58);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(146, 430);
+            button10.Name = "button10";
+            button10.Size = new Size(82, 58);
+            button10.TabIndex = 9;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 20F);
+            textBox1.Location = new Point(29, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(420, 52);
+            textBox1.TabIndex = 10;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(369, 135);
+            button11.Name = "button11";
+            button11.Size = new Size(80, 57);
+            button11.TabIndex = 11;
+            button11.Text = "+";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(369, 240);
+            button12.Name = "button12";
+            button12.Size = new Size(80, 57);
+            button12.TabIndex = 12;
+            button12.Text = "-";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(369, 337);
+            button13.Name = "button13";
+            button13.Size = new Size(80, 57);
+            button13.TabIndex = 13;
+            button13.Text = "/";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(369, 430);
+            button14.Name = "button14";
+            button14.Size = new Size(80, 57);
+            button14.TabIndex = 14;
+            button14.Text = "*";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.Desktop;
+            button15.Location = new Point(260, 430);
+            button15.Name = "button15";
+            button15.Size = new Size(80, 57);
+            button15.TabIndex = 15;
+            button15.Text = "=";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.IndianRed;
+            button16.Location = new Point(29, 430);
+            button16.Name = "button16";
+            button16.Size = new Size(82, 57);
+            button16.TabIndex = 16;
+            button16.Text = "C";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(475, 540);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(textBox1);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private TextBox textBox1;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+    }
+}
---
+++ kalkulator/Form1.cs
@@ -0,0 +1,153 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace kalkulator
+{
+    public partial class Form1 : Form
+    {
+
+        double angka1, angka2, hasil;
+        string operasi;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Input tidak boleh kosong!");
+                return;
+            }
+            operasi = "+";
+            angka1 = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Input tidak boleh kosong!");
+                return;
+            }
+            operasi = "-";
+            angka1 = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Input tidak boleh kosong!");
+                return;
+            }
+            operasi = "/";
+            angka1 = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Input tidak boleh kosong!");
+                return;
+            }
+            operasi = "*";
+            angka1 = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Input tidak boleh kosong!");
+                return;
+            }
+            angka2 = double.Parse(textBox1.Text);
+            switch (operasi)
+            {
+                case "+":
+                    hasil = angka1 + angka2;
+                    textBox1.Text = hasil.ToString();
+                    break;
+
+                case "-":
+                    hasil = angka1 - angka2;
+                    textBox1.Text = hasil.ToString();
+                    break;
+
+                case "/":
+                    hasil = angka1 / angka2;
+                    textBox1.Text = hasil.ToString();
+                    break;
+
+                case "*":
+                    hasil = angka1 * angka2;
+                    textBox1.Text = hasil.ToString();
+                    break;
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+
+    }
+}
